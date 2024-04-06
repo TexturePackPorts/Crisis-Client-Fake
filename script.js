8ba9a42857ecbd69484471dc5670e3b2c81fbfbe
Konradio2009 <konplayzgaming@gmail.com>
2024-04-06 18:36:38 +0200

    updated the website and released the client

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-document.addEventListener("mousemove", function(e) {
-    const mainContent = document.getElementById("mainContent");
-    const xOffset = (window.innerWidth / 2 - e.pageX) / 30;
-    const yOffset = (window.innerHeight / 2 - e.pageY) / 30;
-    mainContent.style.transform = `translate(${xOffset}px, ${yOffset}px)`;
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+    document.getElementById('heading').classList.add('visible');
+  }, 500);
+
+  setTimeout(function() {
+    document.getElementById('description').classList.add('visible');
+  }, 1500);
+
+  setTimeout(function() {
+    document.getElementById('download').classList.add('visible');
+  }, 3000);
+});
